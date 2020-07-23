774a85ffb02249117d834ec6a4f9a0868c6338ae
Andrey Shirokov <shirokov.a.y@gmail.com>
2020-07-24 02:25:38 +0700

    Optimized zeroes removing

--- Algorithms/cpp/611. Valid Triangle Number/solution.h
+++ Algorithms/cpp/611. Valid Triangle Number/solution.h
@@ -1,22 +1,25 @@
+#pragma once
+
 #include <algorithm>
 
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size(); i += 1) {
-            if (nums[i] != 0) {
-                nums.erase(nums.begin(), nums.begin() + i);
+        auto notNullStart = nums.begin();
+        for(; notNullStart < nums.end(); notNullStart += 1) {
+            if (*notNullStart != 0) {
                 break;
             }
         }
-        if (nums.size() < 3) {
+        
+        if ((nums.end() - notNullStart) < 3) {
             return 0;
         }
         
         int result = 0;
         
-        for(auto f = nums.begin(); f != (nums.end() - 2); f += 1) {
+        for(auto f = notNullStart; f != (nums.end() - 2); f += 1) {
             auto s = f + 1;
             auto t = s + 1;
             while (t != nums.end()) {
